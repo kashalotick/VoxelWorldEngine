8417a2487f3b0a4a077ff47d7a3044e2e080da70
Kashalot <priadchenko.dmytro@lll.kpi.ua>
2026-01-11 22:57:00 +0200

    add vector 2 int

---
+++ VoxelWorldEngine/DataStructures/Vector2Int/Vector2Int.cs
@@ -0,0 +1,32 @@
+﻿namespace VoxelWorldEngine.DataStructures.Vector2Int;
+
+public partial struct Vector2Int
+{
+    public int X, Y;
+
+    public const int Count = 2;
+
+    // constructors
+    public Vector2Int(int value)
+    {
+        X = value;
+        Y = value;
+    }
+
+    public Vector2Int(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public static Vector2Int Zero => new(0, 0);
+    public static Vector2Int One => new(1, 1);
+    public static Vector2Int UnitX => new(1, 0);
+    public static Vector2Int UnitY => new(0, 1);
+
+    
+
+    public override int GetHashCode() => HashCode.Combine(X, Y);
+    public override string ToString() => $"{X}, {Y}";
+    
+}
---
+++ VoxelWorldEngine/DataStructures/Vector2Int/Vector2IntOperations.cs
@@ -0,0 +1,32 @@
+﻿namespace VoxelWorldEngine.DataStructures.Vector2Int;
+
+public partial struct Vector2Int : IEquatable<Vector2Int>
+{
+    // equality 
+    public static bool operator ==(Vector2Int left, Vector2Int right) => left.X == right.X && left.Y == right.Y;
+    public static bool operator !=(Vector2Int left, Vector2Int right) => !(left == right);
+    public bool Equals(Vector2Int other) => X == other.X && Y == other.Y;
+    public override bool Equals(object? obj) => obj is Vector2Int other && Equals(other);
+
+
+    // compare 
+
+    public static bool operator >(Vector2Int left, Vector2Int right) => left.LengthSquared() > right.LengthSquared();
+    public static bool operator <(Vector2Int left, Vector2Int right) => left.LengthSquared() < right.LengthSquared();
+
+    // add
+    public static Vector2Int operator +(Vector2Int left, Vector2Int right) => new(left.X + right.X, left.Y + right.Y);
+
+    // subtract
+    public static Vector2Int operator -(Vector2Int left, Vector2Int right) => new(left.X - right.X, left.Y - right.Y);
+    public static Vector2Int operator -(Vector2Int value) => Zero - value;
+
+    // multiply
+    public static Vector2Int operator *(Vector2Int left, Vector2Int right) => new(left.X * right.X, left.Y * right.Y);
+    public static Vector2Int operator *(Vector2Int left, int right) => left * new Vector2Int(right);
+    public static Vector2Int operator *(int left, Vector2Int right) => right * left;
+
+    // divide
+    public static Vector2Int operator /(Vector2Int left, Vector2Int right) => new(left.X / right.X, left.Y / right.Y);
+    public static Vector2Int operator /(Vector2Int left, int right) => left / new Vector2Int(right);
+}
