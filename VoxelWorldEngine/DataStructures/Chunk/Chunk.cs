855793aa15237b572a9963739f90b8edaab75fa9
Kashalot <priadchenko.dmytro@lll.kpi.ua>
2026-01-13 23:53:37 +0200

    refactor(chunk): prepare for octree builder testing

--- VoxelWorldEngine/DataStructures/Chunk/Chunk.cs
+++ VoxelWorldEngine/DataStructures/Chunk/Chunk.cs
@@ -1,7 +1,18 @@
 ﻿namespace VoxelWorldEngine.DataStructures.Chunk;
 
+// TODO: implement logic
 public class Chunk
 {
     public Vector3Int.Vector3Int Position;
     public LinearOctree.LinearOctree Octree;
+
+    public Chunk(Vector3Int.Vector3Int position)
+    {
+        Position = position;
+    }
+
+    public void SetOctree(LinearOctree.LinearOctree octree)
+    {
+        Octree = octree;
+    }
 }
