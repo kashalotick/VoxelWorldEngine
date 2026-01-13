34a8aa99dc2c7e25dc167a985d6d674c6c9a924b
Kashalot <priadchenko.dmytro@lll.kpi.ua>
2026-01-13 23:41:34 +0200

    feat(octree builder): complete node building implement node processing and improve density generation handling
    - Add `ProcessNode`, `ProcessSubdivide`, and `ProcessLeaf` methods.
    - Replace `DensityGenerator` with `IScalarFieldGenerator` for dependency injection.
    - Add XML documentation.
    - Refactor chunk-to-octree building logic for clarity.

--- VoxelWorldEngine/Core/Builder/OctreeBuilder.cs
+++ VoxelWorldEngine/Core/Builder/OctreeBuilder.cs
@@ -1,4 +1,4 @@
-﻿using VoxelWorldEngine.Core.Generator;
+﻿using VoxelWorldEngine.Core.Generators.Interfaces;
 using VoxelWorldEngine.DataStructures.Chunk;
 using VoxelWorldEngine.DataStructures.LinearOctree;
 using VoxelWorldEngine.DataStructures.Vector3Int;
@@ -6,73 +6,119 @@ using VoxelWorldEngine.DataStructures.Voxel;
 
 namespace VoxelWorldEngine.Core.Builder;
 
+// TODO: make tests
+// TODO: add Observer class + observer field + distance check
+/// <summary>
+///     The OctreeBuilder class provides functionality to construct a LinearOctree structure
+///     using density data generated within a specified chunk. It acts as a bridge between
+///     the chunk-based representation and the octree-based representation of a voxel world.
+/// </summary>
 public class OctreeBuilder
 {
-    private DensityGenerator _densityGenerator;
+    private readonly IScalarFieldGenerator<Vector3Int, sbyte> _densityGenerator;
 
-    public OctreeBuilder()
+    public OctreeBuilder(IScalarFieldGenerator<Vector3Int, sbyte> densityGenerator)
     {
-        _densityGenerator = new DensityGenerator();
+        _densityGenerator = densityGenerator;
     }
 
 
+    /// <summary>
+    ///     Constructs a linear octree from the density data of the provided chunk.
+    /// </summary>
+    /// <param name="chunk">The chunk, from which the octree will be constructed.</param>
+    /// <returns>A LinearOctree instance.</returns>
     public LinearOctree Build(Chunk chunk)
     {
         var octree = new LinearOctree();
 
-        var indent = LinearOctree.MaxDepth;
-        var min = ChunkMath.ChunkToGlobal(chunk.Position);
-        
-        var nodeSize = 1 << indent;
-        var max = min + Vector3Int.One * nodeSize;
+        var nodeSize = 1 << LinearOctree.MaxDepth;
+        var parentMin = ChunkMath.ChunkToGlobal(chunk.Position);
 
-        var nodeIndex = octree.RootIndex;
-        if (ShouldSubdivideRegion(min, max) && nodeSize > 1)
-        {
-            nodeSize >>= 1;
-            var firstChildIndex = octree.SubdivideNode(nodeIndex);
-            for (int i = 0; i < 8; i++)
-            {
-                min = min;
-                max = min + Vector3Int.One * nodeSize;
-                // var childMin
-                // var childMax
-            }
-        }
-        else
-        {
-            ProcessLeaf(min, max, octree, nodeIndex);
-        }
-            
+        ProcessNode(octree.RootIndex, nodeSize, octree, parentMin);
+
         return octree;
     }
 
-    private void ProcessLeaf(Vector3Int min, Vector3Int max, LinearOctree octree, int nodeIndex)
+    /// <summary>
+    ///     Processing node on a specified index.
+    /// </summary>
+    /// <param name="nodeIndex">Node index in node list.</param>
+    /// <param name="nodeSize">Node size - power of 2.</param>
+    /// <param name="octree">Building octree.</param>
+    /// <param name="octantCorner">Position of 0's corner of octant.</param>
+    private void ProcessNode(int nodeIndex, int nodeSize, LinearOctree octree, Vector3Int octantCorner)
+    {
+        if (ShouldSubdivideNode(nodeSize, octantCorner) && nodeSize > 1)
+        {
+            ProcessSubdivide(nodeIndex, nodeSize, octree, octantCorner);
+        }
+        else
+        {
+            ProcessLeaf(nodeIndex, nodeSize, octree, octantCorner);
+        }
+    }
+
+    /// <summary>
+    ///     Processing subdividing of node on a specified index.
+    /// </summary>
+    /// <param name="nodeIndex">Node index in node list.</param>
+    /// <param name="nodeSize">Node size - power of 2.</param>
+    /// <param name="octree">Building octree.</param>
+    /// <param name="octantCorner">Position of 0's corner of octant.</param>
+    private void ProcessSubdivide(int nodeIndex, int nodeSize, LinearOctree octree, Vector3Int octantCorner)
+    {
+        var firstChildIndex = octree.SubdivideNode(nodeIndex);
+        var childNodeSize = nodeSize >> 1;
+
+        for (var i = 0; i < 8; i++)
+        {
+            var octantLocalPosition = OctreeMath.GetOctantCorner(i, nodeSize);
+            var childOctantCorner = octantCorner + octantLocalPosition;
+
+            var childNodeIndex = firstChildIndex + i;
+            ProcessNode(childNodeIndex, childNodeSize, octree, childOctantCorner);
+        }
+    }
+
+    /// <summary>
+    ///     Processing leaf node on a specified index.
+    /// </summary>
+    /// <param name="nodeIndex">Node index in node list.</param>
+    /// <param name="nodeSize">Node size - power of 2.</param>
+    /// <param name="octree">Building octree.</param>
+    /// <param name="octantCorner">Position of 0's corner of octant.</param>
+    private void ProcessLeaf(int nodeIndex, int nodeSize, LinearOctree octree, Vector3Int octantCorner)
     {
         // TODO: remaker with avg density ???
-        var center = (min + max) / 2;
-        sbyte density = _densityGenerator.GetValue(center);
-            
+
+        var center = nodeSize > 1 ? octantCorner + Vector3Int.One * (nodeSize / 2) : octantCorner;
+        var density = _densityGenerator.GetValue(center);
+
         var voxel = new Voxel(density);
         octree.SetNodeVoxel(nodeIndex, voxel);
     }
 
-
-    private void Subdivide()
+    /// <summary>
+    ///     Calculates whether the node should be subdivided or not.
+    /// </summary>
+    /// <param name="nodeSize">Node size - power of 2.</param>
+    /// <param name="octantCorner">Position of 0's corner of octant.</param>
+    /// <returns>True if should subdivide.</returns>
+    private bool ShouldSubdivideNode(int nodeSize, Vector3Int octantCorner)
     {
-        throw new NotImplementedException();
-    }
+        var octantLastCorner = octantCorner + Vector3Int.One * nodeSize;
 
-    private bool ShouldSubdivideRegion(Vector3Int a, Vector3Int b)
-    {
-        var regionSize = Vector3Int.Abs(a - b).X + 1;
-        var indent = int.Log2(regionSize);
-        // in indent?
-        // out density?
-        
+        if (nodeSize > 1)
+        {
+            // var indent = int.Log2(regionSize);
+            // out density?
+            // TODO: add distance to observer check
 
-        var isHereSurface = _densityGenerator.IsHereAnySurface(a, b);
-        
-        return isHereSurface;
+            var isHereSurface = _densityGenerator.IsHereAnySurface(octantCorner, octantLastCorner);
+
+            return isHereSurface;
+        }
+        return false;
     }
 }
