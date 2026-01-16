543f4988455c00ccd68177234cf9ac637db1aa3d
Kashalotik <priadchenko.dmytro@lll.kpi.ua>
2026-01-16 16:36:07 +0200

    wip refactor(mesh builder): make face calculations with matrix transformations

--- VoxelWorldEngine/Core/Builders/MeshBuilder.cs
+++ VoxelWorldEngine/Core/Builders/MeshBuilder.cs
@@ -1,6 +1,8 @@
-﻿using VoxelWorldEngine.DataStructures.LinearOctree;
+﻿using System.Numerics;
+using VoxelWorldEngine.DataStructures.LinearOctree;
 using VoxelWorldEngine.DataStructures.Mesh;
 using VoxelWorldEngine.DataStructures.Vector3Int;
+using VoxelWorldEngine.Utils;
 
 namespace VoxelWorldEngine.Core.Builders;
 
@@ -25,7 +27,7 @@ public class MeshBuilder
         while (queue.Count > 0)
         {
             cycleCount++;
-            
+
             var meshNode = queue.Dequeue();
             var octreeNode = octree.GetNode(meshNode.Index);
 
@@ -37,14 +39,12 @@ public class MeshBuilder
             {
                 for (int i = 0; i < 8; i++)
                 {
-                    
-
                     var childIndex = octreeNode.GetChildIndex(i);
                     var childSize = meshNode.Size >> 1;
                     var localOffset = OctreeMath.GetOctantCorner(i, meshNode.Size);
                     var position = meshNode.Position + localOffset;
-                    
-                    var childMeshNode =  new MeshNode(childIndex, childSize, position);
+
+                    var childMeshNode = new MeshNode(childIndex, childSize, position);
                     queue.Enqueue(childMeshNode);
                 }
             }
@@ -58,7 +58,7 @@ public class MeshBuilder
     {
         var octreeNode = octree.GetNode(node.Index);
         if (octreeNode.IsAir) return;
-        
+
         foreach (var direction in _directions)
         {
             if (IsFaceVisible(node, direction, octree))
@@ -68,50 +68,75 @@ public class MeshBuilder
         }
     }
 
+    // TODO: test new impl; remake with bounds; impl bounds iterator in octree
     private bool IsFaceVisible(MeshNode node, Vector3Int direction, LinearOctree octree)
     {
         // Перевіряємо всю грань, а не одну точку
         var size = node.Size;
-    
-        // Визначаємо по якій осі йде напрямок
-        if (direction.X != 0)
+        // var halfSize = size >> 1;
+
+
+
+        var newOrigin = (node.Position.ToVector3() + Vector3.One * (size - 1)) / 2;
+        var transformMatrix = VectorHelper.GetTransformMatrix(newOrigin, direction.ToVector3());
+        //
+        // var minBound = new Vector3Int(0, 0, size);
+        // var maxBound = new Vector3Int(size - 1, size - 1, size);
+        // minBound = VectorHelper.Transform(minBound, transformMatrix);
+        // maxBound = VectorHelper.Transform(maxBound, transformMatrix);
+        // if (IsAirInArea(minBound, maxBound, octree))
+        // {
+        //     return true;
+        // }
+        //
+        for (int x = 0; x < size; x++)
+        for (int y = 0; y < size; y++)
         {
-            // Грань перпендикулярна X
-            var checkX = node.Position.X + (direction.X > 0 ? size : -1);
+            var checkPos = new Vector3Int(x, y, size);
+            checkPos = VectorHelper.Transform(checkPos, transformMatrix);
+            if (IsPositionAir(checkPos, octree))
+                return true; 
+        }
         
-            for (int y = 0; y < size; y++)
-            for (int z = 0; z < size; z++)
-            {
-                var checkPos = new Vector3Int(checkX, node.Position.Y + y, node.Position.Z + z);
-                if (IsPositionAir(checkPos, octree))
-                    return true; // Хоч одна точка грані видима
-            }
-        }
-        else if (direction.Y != 0)
-        {
-            var checkY = node.Position.Y + (direction.Y > 0 ? size : -1);
-            
-            for (int x = 0; x < size; x++)
-            for (int z = 0; z < size; z++)
-            {
-                var checkPos = new Vector3Int(node.Position.X + x, checkY, node.Position.Z + z);
-                if (IsPositionAir(checkPos, octree))
-                    return true;
-            }
-        }
-        else if (direction.Z != 0)
-        {
-            var checkZ = node.Position.Z + (direction.Z > 0 ? size : -1);
-        
-            for (int x = 0; x < size; x++)
-            for (int y = 0; y < size; y++)
-            {
-                var checkPos = new Vector3Int(node.Position.X + x, node.Position.Y + y, checkZ);
-                if (IsPositionAir(checkPos, octree))
-                    return true;
-            }
-        }
-    
+        // // Визначаємо по якій осі йде напрямок
+        // if (direction.X != 0)
+        // {
+        //     // Грань перпендикулярна X
+        //     var checkX = node.Position.X + (direction.X > 0 ? size : -1);
+        //
+        //     for (int y = 0; y < size; y++)
+        //     for (int z = 0; z < size; z++)
+        //     {
+        //         var checkPos = new Vector3Int(checkX, node.Position.Y + y, node.Position.Z + z);
+        //         if (IsPositionAir(checkPos, octree))
+        //             return true; // Хоч одна точка грані видима
+        //     }
+        // }
+        // else if (direction.Y != 0)
+        // {
+        //     var checkY = node.Position.Y + (direction.Y > 0 ? size : -1);
+        //
+        //     for (int x = 0; x < size; x++)
+        //     for (int z = 0; z < size; z++)
+        //     {
+        //         var checkPos = new Vector3Int(node.Position.X + x, checkY, node.Position.Z + z);
+        //         if (IsPositionAir(checkPos, octree))
+        //             return true;
+        //     }
+        // }
+        // else if (direction.Z != 0)
+        // {
+        //     var checkZ = node.Position.Z + (direction.Z > 0 ? size : -1);
+        //
+        //     for (int x = 0; x < size; x++)
+        //     for (int y = 0; y < size; y++)
+        //     {
+        //         var checkPos = new Vector3Int(node.Position.X + x, node.Position.Y + y, checkZ);
+        //         if (IsPositionAir(checkPos, octree))
+        //             return true;
+        //     }
+        // }
+
         return false; // Вся грань закрита
     }
 
@@ -119,11 +144,10 @@ public class MeshBuilder
     {
         if (!position.IsInBounds(LinearOctree.Size))
             return true;
-        
+
         var node = octree.GetNode(position);
-        return node.IsAir;
+        return !node.IsSolid;
     }
-    
 }
 
 public struct MeshNode
--- VoxelWorldEngine/DataStructures/Mesh/Mesh.cs
+++ VoxelWorldEngine/DataStructures/Mesh/Mesh.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using VoxelWorldEngine.Utils;
 
 namespace VoxelWorldEngine.DataStructures.Mesh;
 
@@ -42,55 +43,19 @@ public class Mesh
         Vector3[] vertices = new Vector3[4];
         Vector3 pos = new Vector3(position.X, position.Y, position.Z);
         float s = size;
+        var halfSize = size / 2;
 
-        // Передня грань (Z+)
-        if (normal.Z == 1)
-        {
-            vertices[0] = pos + new Vector3(0, 0, s);
-            vertices[1] = pos + new Vector3(s, 0, s);
-            vertices[2] = pos + new Vector3(s, s, s);
-            vertices[3] = pos + new Vector3(0, s, s);
-        }
-        // Задня грань (Z-)
-        else if (normal.Z == -1)
-        {
-            vertices[0] = pos + new Vector3(s, 0, 0);
-            vertices[1] = pos + new Vector3(0, 0, 0);
-            vertices[2] = pos + new Vector3(0, s, 0);
-            vertices[3] = pos + new Vector3(s, s, 0);
-        }
-        // Права грань (X+)
-        else if (normal.X == 1)
-        {
-            vertices[0] = pos + new Vector3(s, 0, s);
-            vertices[1] = pos + new Vector3(s, 0, 0);
-            vertices[2] = pos + new Vector3(s, s, 0);
-            vertices[3] = pos + new Vector3(s, s, s);
-        }
-        // Ліва грань (X-)
-        else if (normal.X == -1)
-        {
-            vertices[0] = pos + new Vector3(0, 0, 0);
-            vertices[1] = pos + new Vector3(0, 0, s);
-            vertices[2] = pos + new Vector3(0, s, s);
-            vertices[3] = pos + new Vector3(0, s, 0);
-        }
-        // Верхня грань (Y+)
-        else if (normal.Y == 1)
-        {
-            vertices[0] = pos + new Vector3(0, s, s);
-            vertices[1] = pos + new Vector3(s, s, s);
-            vertices[2] = pos + new Vector3(s, s, 0);
-            vertices[3] = pos + new Vector3(0, s, 0);
-        }
-        // Нижня грань (Y-)
-        else if (normal.Y == -1)
-        {
-            vertices[0] = pos + new Vector3(0, 0, 0);
-            vertices[1] = pos + new Vector3(s, 0, 0);
-            vertices[2] = pos + new Vector3(s, 0, s);
-            vertices[3] = pos + new Vector3(0, 0, s);
-        }
+        var transformMatrix = VectorHelper.GetTransformMatrix(pos, normal.ToVector3());
+        
+        var localVertex0 = new Vector3(-halfSize, -halfSize, halfSize);
+        var localVertex1 = new Vector3(halfSize, -halfSize, halfSize);
+        var localVertex2 = new Vector3(halfSize, halfSize, halfSize);
+        var localVertex3 = new Vector3(-halfSize, halfSize, halfSize);
+
+        vertices[0] = Vector3.Transform(localVertex0, transformMatrix);
+        vertices[1] = Vector3.Transform(localVertex1, transformMatrix);
+        vertices[2] = Vector3.Transform(localVertex2, transformMatrix);
+        vertices[3] = Vector3.Transform(localVertex3, transformMatrix);
 
         return vertices;
     }
