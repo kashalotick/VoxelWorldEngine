2693970141021beeef222dc56b708274467e0ce9
Kashalot <priadchenko.dmytro@lll.kpi.ua>
2026-01-16 22:55:47 +0200

    chore(min max avg): make struct for future refactor

---
+++ VoxelWorldEngine/DataStructures/MinMaxAvg.cs
@@ -0,0 +1,15 @@
+﻿namespace VoxelWorldEngine.DataStructures;
+
+public struct MinMaxAvg<T>
+{
+    public T Min;
+    public T Max;
+    public T Avg;
+
+    public MinMaxAvg(T min, T max, T avg)
+    {
+        Min = min;
+        Max = max;
+        Avg = avg;
+    }
+}
