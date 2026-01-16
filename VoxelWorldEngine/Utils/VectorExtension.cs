3ab8137dab59eced15127f8afbd4036f5c03de17
Kashalotik <priadchenko.dmytro@lll.kpi.ua>
2026-01-16 16:34:58 +0200

    feat(vector helper): add class for matrix transformation

---
+++ VoxelWorldEngine/Utils/VectorExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+using VoxelWorldEngine.DataStructures.Vector2Int;
+using VoxelWorldEngine.DataStructures.Vector3Int;
+
+namespace VoxelWorldEngine.Utils;
+
+public static class VectorExtension
+{
+    public static Vector3Int ToVector3Int(this Vector3 vector) => new ((int)vector.X, (int)vector.Y, (int)vector.Z);
+    public static Vector2Int Vector2Int(Vector2 vector) => new ((int)vector.X, (int)vector.Y);
+}
--- VoxelWorldEngine/Utils/VectorHelper.cs
+++ VoxelWorldEngine/Utils/VectorHelper.cs
@@ -1,11 +1,52 @@
 ﻿using System.Numerics;
+using VoxelWorldEngine.DataStructures.Vector3Int;
 
 namespace VoxelWorldEngine.Utils;
 
+// TODO: tests
 public static class VectorHelper
 {
-    public static Vector3 RotateToNormal(Vector3 vector, Vector3 normal)
+    public static Vector3Int Transform(Vector3Int vector, Matrix4x4 matrix)
     {
-        return Vector3.Cross(normal, vector);
+        var vec3 = vector.ToVector3();
+        var transformed = Vector3.Transform(vec3, matrix);
+        var result = transformed.ToVector3Int();
+
+        return result;
+    }
+
+    public static Matrix4x4 GetTransformMatrix(Vector3Int newOrigin, Vector3Int newZAxis,
+        Vector3Int oldZAxis = default, Vector3Int oldXAxis = default)
+    {
+        return GetTransformMatrix(newOrigin.ToVector3(), newZAxis.ToVector3(), oldZAxis.ToVector3(),
+            oldXAxis.ToVector3());
+    }
+
+    public static Matrix4x4 GetTransformMatrix(
+        Vector3 newOrigin, Vector3 newZAxis, 
+        Vector3 oldZAxis = default, Vector3 oldXAxis = default
+        ) 
+    {
+        if (oldZAxis == default) oldZAxis = Vector3.UnitZ;
+        if (oldXAxis == default) oldXAxis = Vector3.UnitX;
+
+        Vector3 newXAxis;
+        
+        if (newZAxis == oldZAxis) newXAxis = oldXAxis;
+        else if (newZAxis == -oldZAxis) newXAxis = -oldXAxis;
+        else newXAxis = Vector3.Cross(oldZAxis, newZAxis);
+        
+        var result  = new Matrix4x4();
+        
+        var axisZ = Vector3.Normalize(newZAxis);
+        var axisX = Vector3.Normalize(newXAxis);
+        var axisY = Vector3.Cross(axisZ, axisX);
+        
+        result.X = axisX.AsVector4();
+        result.Y = axisY.AsVector4();
+        result.Z = axisZ.AsVector4();
+        result.W = Vector4.Create(newOrigin, 1);
+
+        return result;
     }
 }
