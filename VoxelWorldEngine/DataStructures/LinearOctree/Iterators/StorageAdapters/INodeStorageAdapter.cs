00fd72899051cb6aeae8375ff72293fd684e1a2d
Kashalot <priadchenko.dmytro@lll.kpi.ua>
2026-01-19 15:48:25 +0200

    chore(storage adapters): add queue and stack adapter for BFS and DFS octree iterator

---
+++ VoxelWorldEngine/DataStructures/LinearOctree/Iterators/StorageAdapters/INodeStorageAdapter.cs
@@ -0,0 +1,9 @@
+﻿namespace VoxelWorldEngine.DataStructures.LinearOctree.Iterators.StorageAdapters;
+
+public interface INodeStorageAdapter<T>
+{
+    void Add(T item);
+    T GetAndRemove();
+    bool IsEmpty();
+    void Clear();
+}
---
+++ VoxelWorldEngine/DataStructures/LinearOctree/Iterators/StorageAdapters/QueueAdapter.cs
@@ -0,0 +1,11 @@
+﻿namespace VoxelWorldEngine.DataStructures.LinearOctree.Iterators.StorageAdapters;
+
+public class QueueAdapter<T> : INodeStorageAdapter<T>
+{
+    private Queue<T> _queue = new Queue<T>();
+
+    public void Add(T item) => _queue.Enqueue(item);
+    public T GetAndRemove() => _queue.Dequeue();
+    public bool IsEmpty() => _queue.Count == 0;
+    public void Clear() => _queue.Clear();
+}
