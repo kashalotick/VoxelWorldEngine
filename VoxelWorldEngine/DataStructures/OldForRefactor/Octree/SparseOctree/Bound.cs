4d9b57ed2ad032b55410ab06ea0abcaac85f64d3
Kashalot <priadchenko.dmytro@lll.kpi.ua>
2026-02-10 16:30:25 +0200

    merge resolve

--- VoxelWorldEngine/DataStructures/OldForRefactor/Octree/SparseOctree/Bound.cs
+++ VoxelWorldEngine/DataStructures/OldForRefactor/Octree/SparseOctree/Bound.cs
@@ -1,9 +1,9 @@
+using VoxelWorldEngine.DataStructures.Common.Structures.Vectors;
+
 namespace VoxelWorldEngine.DataStructures.Octree.SparseOctree;
 
 public struct Bound
 {
-    public Vector3Int.Vector3Int Min;
-    public Vector3Int.Vector3Int Max;
-    
-    
+    public Vector3Int Min;
+    public Vector3Int Max;
 }
--- VoxelWorldEngine/DataStructures/OldForRefactor/Octree/SparseOctree/Composite.cs
+++ VoxelWorldEngine/DataStructures/OldForRefactor/Octree/SparseOctree/Composite.cs
@@ -6,7 +6,31 @@ public class SparseOctreeNode : IOctreeNode<NodeData>
     public int Depth { get; set; }
     public bool IsLeaf { get; }
     public NodeData Data { get; set; }
-    
+    public void Insert(IOctreeNode<NodeData> node)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Apply(Bound region, Action<NodeData> operation)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<IOctreeNode<NodeData>> Query(Bound region)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Split()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TryMerge()
+    {
+        throw new NotImplementedException();
+    }
+
     private readonly List<IOctreeNode<NodeData?>> _children = new List<IOctreeNode<NodeData?>>();
     private byte _childMask = 0b_0000_0000;
     private int _count;
